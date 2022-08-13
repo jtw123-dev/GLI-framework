a75b413d33a9e2df7a78b20b2243e0987fe0e0a8
jtw123-dev <78871610+jtw123-dev@users.noreply.github.com>
2022-08-13 09:45:56 -0230

    Added start point

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+    [SerializeField] private Transform _startingPos;
+   // [SerializeField] private Transform _endPos;
+    private NavMeshAgent _agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // _agent.SetDestination(_transformB.position);
+    }
+}
